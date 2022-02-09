83f8fd86e1b6717e38d1cf3323e933b17e21af2b
Coduet96 <pathakvanshdev@gmail.com>
2022-02-09 16:25:20 +0530

    day1

---
+++ python/python_lists.py
@@ -0,0 +1,6 @@
+
+# lists are python equivalent of arrays
+
+stars = ["Ramos", "Ronaldo", "Benzema" ]
+
+print(stars)
---
+++ python/python_variables.py
@@ -0,0 +1,10 @@
+first_name = input("Please enter your first name : ")
+last_name = input("Please enter your last name : ")
+name = first_name + " " + last_name
+name= name.lower()
+print("Hello, " + name + "!!!!!")
+
+age = int(input(first_name+ " please enter your age : "))
+print(first_name +", you will be " + str(age + 5) +" in 5 years")
+
+
